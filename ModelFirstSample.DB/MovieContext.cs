6b188185cc1480a4718a1048cd49606a30480274
glasista <ruszala.rafal@gmail.com>
2020-01-26 05:28:27 +0100

    Adding reference  to DB project and preparing DbContext

---
+++ ModelFirstSample.DB/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelFirstSample.DB
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}
---
+++ ModelFirstSample.DB/MovieContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelFirstSample.DB
+{
+    public class MovieContextFactory : IDbContextFactory<MovieContext>
+    {
+        public MovieContext Create()
+        {
+            return new MovieContext("Server=(localdb)\\mssqllocaldb;Database=ModelFirstSample;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
